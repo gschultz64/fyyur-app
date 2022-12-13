d24b4ad3a1a747654120f90e172c1da22f24f6d7
Genevieve Schultz <genschultz@gmail.com>
2022-12-12 22:33:12 -0800

    models and migrations setup

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://student:student@localhost:5432/fyyur'
